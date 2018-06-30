6b3092569065449ee45632b8f16f23f21503d5ee
vannyy01 <vannyy01@gmail.com>
2018-06-30 15:41:39 +0300

    privet

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'hello world'});
+    res.send({privet: 'hello world'});
 });
 
 const PORT = process.env.PORT || 5000;
