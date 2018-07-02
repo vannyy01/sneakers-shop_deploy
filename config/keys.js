0788fa6865928a15c377ca2248da66a15ea8159a
vannyy01 <vannyy01@gmail.com>
2018-07-02 20:42:14 +0300

    v 1.3

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+if (process.env.NODE_ENV === 'production') {
+    // return product set of keys
+    module.exports = require('./prod');
+} else {
+    // return dev set of keys
+    module.exports = require('./dev');
+}
