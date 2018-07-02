ab56658dca82d47fb1b87a195699109cf51dbcc4
vannyy01 <vannyy01@gmail.com>
2018-07-02 20:16:21 +0300

    Prod env v 1.0

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: "517980104685-10jev64ft82hmhm88ic5vsvje8hlpgq1.apps.googleusercontent.com",
+    googleClientSecret: "HJH4JK-2szOAvXRWuI8luoCk",
+    mongoDBConnect: "mongodb://root:root1234@ds125181.mlab.com:25181/sneekers-shop-dev",
+    cookieKey: "wjbejfafdksfdmsksdf2423"
+};
